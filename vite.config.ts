b97780609351dd3fbdf5342e279c847f22f7fbce
mohamed ali <ffgg35646@gmail.com>
2026-07-12 18:15:34 -0700

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,30 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
+export default defineConfig({
+  plugins: [react()],
+
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    open: true,
+  },
+
+  preview: {
+    host: "0.0.0.0",
+    port: 4173,
+  },
+
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    emptyOutDir: true,
+  },
+});
