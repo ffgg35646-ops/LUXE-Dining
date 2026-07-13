0afb4be9ccb7e2e188c0b6a3b694e6ca744c3911
mohamed ali <ffgg35646@gmail.com>
2026-07-12 23:43:41 -0700

    Home.tsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,26 @@
+import Hero from "@/components/home/Hero";
+import FeaturedDishes from "@/components/home/FeaturedDishes";
+import Categories from "@/components/home/Categories";
+import ChefSection from "@/components/home/ChefSection";
+import Gallery from "@/components/home/Gallery";
+import Testimonials from "@/components/home/Testimonials";
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+
+      <FeaturedDishes />
+
+      <Categories />
+
+      <ChefSection />
+
+      <Gallery />
+
+      <Testimonials />
+    </>
+  );
+};
+
+export default Home;
